500502742397570299be2c2b7f50a6e9c64de308
723poil <leetkdguq73@naver.com>
2022-11-27 22:39:46 +0900

    Update files, api about classification and install child_process module

--- nodejs/routes/image.js
+++ nodejs/routes/image.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const crypto = require('crypto')
 const multer = require('multer')
+const spawn = require('child_process').spawn
 
 const storage = multer.diskStorage({
     destination: 'images/',
@@ -15,6 +16,65 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({ storage: storage})
+
+router.post('/classification', upload.single('file'), (req, res) => {
+    try {
+        let file = req.file
+        let originalname = ''
+        let fileName = ''
+        let mimeType = ''
+        let size = 0
+        let result
+
+        if (file) {
+            originalname = file.originalname
+            fileName = file.filename
+            mimeType = file.mimeType
+            size = file.size
+
+            result = spawn('python', ['../dlengine/classification.py', fileName])
+        }
+
+        console.log('originalname : ' + originalname + 
+                ', \nfileName : ' + fileName + 
+                ', \nmimeType : ' + mimeType + 
+                ', \nsize : ' + size)
+        
+        result.stdout.on('data', (data) => {
+            console.log(fileName + ' result : ' + data.toString())
+            res.send({
+                'originalname' : originalname,
+                'fileName' : fileName,
+                'mimeType' : mimeType,
+                'size' : size,
+                'status' : true,
+                'result' : data.toString()
+            })
+        })
+
+        result.stderr.on('data', (data) => {
+            console.log(fileName + ' result : ' + data.toString())
+            res.send({
+                'originalname' : originalname,
+                'fileName' : fileName,
+                'mimeType' : mimeType,
+                'size' : size,
+                'status' : true,
+                'result' : 'no result'
+            })
+        })
+    } catch (err) {
+        console.log(err)
+        res.send({
+            'originalname' : '',
+            'fileName' : '',
+            'mimeType' : '',
+            'size' : 0,
+            'status' : false,
+            'result' : 'no result'
+        })
+    }
+})
 
 router.post('/', upload.single('file'), (req, res) => {
     try {
