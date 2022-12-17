271f9ad430e8e09f1adf9099e74f21d06c449414
723poil <leetkdguq73@naver.com>
2022-12-17 18:35:08 +0900

    Remove test code (console.log etc..)

--- nodejs/routes/feedback.js
+++ nodejs/routes/feedback.js
@@ -3,37 +3,34 @@ const imgModel = require('../models/imgmodel')
 
 router.post('/update_validity', (req, res) => {
     var imgid = req.body.imgid
-	console.log(imgid)
+	
 	var bool_ = (req.body.bool == undefined)? false : req.body.bool
-	imgModel.findOneAndUpdate({'imageName': imgid}, {$set: {'rightYn': bool_}}
-		).then(response => {
-			console.log('111')
-			console.log(response)
-		}).catch(err => {
-			console.log('222')
-			console.log(err)
-
+	imgModel.findOneAndUpdate({'imageName': imgid}, {$set: {'rightYn': bool_}}).then(response => {
+		res.send({
+			'result': true
 		})
-		res.send({'aaa': imgid, 'bbb': imgModel})
+	}).catch(err => {
+		res.send({
+			'result': false
+		})
 
+	})
 })
-
-router.get('/aaaa', (req, res) => res.send({'test': 'Hello World'}))
 
 router.post('/remove', (req, res) => {
     var imgid = req.body.imgid
 	console.log(imgid)
 	var bool_ = false
-	imgModel.findOneAndRemove({'imageName': imgid})
-		.then(response => {
-			console.log('111')
-			console.log(response)
-		}).catch(err => {
-			console.log('222')
-			console.log(err)
-
+	imgModel.findOneAndRemove({'imageName': imgid}).then(response => {
+		res.send({
+			'result': true
 		})
-		res.send({'aaa': imgid, 'bbb': imgModel})
+	}).catch(err => {
+		res.send({
+			'result': false
+		})
+
+	})
 })
 
 module.exports = router
