4813b16fb71813bae408ac28706725df0153681d
jihyenbae <biod0424@knu.ac.kr>
2022-12-19 17:26:12 +0900

    Update Toolbar, BottomNav, DrawerNav Transaction search, home, mypage

--- app/src/main/java/com/moapp/letyouknowrecyclingapp/MainActivity.kt
+++ app/src/main/java/com/moapp/letyouknowrecyclingapp/MainActivity.kt
@@ -1,11 +1,115 @@
 package com.moapp.letyouknowrecyclingapp
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
+import com.moapp.letyouknowrecyclingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    //드로어
+    lateinit var toggle: ActionBarDrawerToggle
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //툴바
+        setSupportActionBar(binding.toolbar)
+
+        //시작이 HomeFragment로 화면이 대체됨
+        supportFragmentManager.beginTransaction().replace(R.id.frame_layout, MainHomeFragment()).commit()
+
+        //ActionBarDrawerToggle 버튼 적용
+        toggle = ActionBarDrawerToggle(this, binding.drawer, 0, 0)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toggle.syncState()
+
+        //드로어 아이템 선택 리스너
+        binding.drawerView.setNavigationItemSelectedListener() {
+            when(it.itemId) {
+
+                R.id.item1 -> {
+                    var intent = Intent(this, MainMypageSetting1Activity::class.java)
+                    startActivity(intent)
+                    binding.drawer.closeDrawer(GravityCompat.START)
+                    return@setNavigationItemSelectedListener true
+                }
+
+                R.id.item2 -> {
+                    var intent = Intent(this, MainMypageSetting1Activity::class.java)
+                    startActivity(intent)
+                    binding.drawer.closeDrawer(GravityCompat.START)
+                    return@setNavigationItemSelectedListener true
+                }
+
+                R.id.item3 -> {
+                    var intent = Intent(this, MainMypageSetting1Activity::class.java)
+                    startActivity(intent)
+                    binding.drawer.closeDrawer(GravityCompat.START)
+                    return@setNavigationItemSelectedListener true
+                }
+
+                R.id.item4 -> {
+                    var intent = Intent(this, MainMypageSetting1Activity::class.java)
+                    startActivity(intent)
+                    binding.drawer.closeDrawer(GravityCompat.START)
+                    return@setNavigationItemSelectedListener true
+                }
+
+                R.id.item5 -> {
+                    var intent = Intent(this, MainMypageSetting1Activity::class.java)
+                    startActivity(intent)
+                    binding.drawer.closeDrawer(GravityCompat.START)
+                    return@setNavigationItemSelectedListener true
+                }
+
+
+            }
+            return@setNavigationItemSelectedListener true
+        }
+
+        //바텀바 아이템 선택 리스터 (Fragment)
+        binding.bottomNavView.setOnItemSelectedListener {
+            when (it.itemId) {
+
+                R.id.search -> supportFragmentManager.beginTransaction().replace(R.id.frame_layout, MainSearchFragment()).addToBackStack(null).commit()
+                //R.id.search -> replaceFragment(MainSearchFragment())
+                R.id.home -> supportFragmentManager.beginTransaction().replace(R.id.frame_layout, MainHomeFragment()).addToBackStack(null).commit()
+                //R.id.home -> replaceFragment(MainHomeFragment())
+                R.id.mypage -> supportFragmentManager.beginTransaction().replace(R.id.frame_layout, MainMypageFragment()).addToBackStack(null).commit()
+                //R.id.mypage -> replaceFragment(MainMypageFragment())
+
+
+                else -> {
+
+                }
+
+            }
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/com/moapp/letyouknowrecyclingapp/MainMypageSetting1Activity.kt
@@ -0,0 +1,19 @@
+package com.moapp.letyouknowrecyclingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.moapp.letyouknowrecyclingapp.databinding.ActivityMainMypageSetting1Binding
+
+class MainMypageSetting1Activity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainMypageSetting1Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainMypageSetting1Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.backBtn.setOnClickListener(){
+            onBackPressed()
+        }
+    }
+}
